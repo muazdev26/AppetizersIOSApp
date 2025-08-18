1d0d655ef6f884d393cba3dc47eb6f18cf319081
muazdev <funzoft.muaz@gmail.com>
2025-08-18 12:37:25 +0500

    Initial Commit

---
+++ EcommerceIOSApp/EcommerceIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceIOSAppApp.swift
+//  EcommerceIOSApp
+//
+//  Created by Muaz on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
